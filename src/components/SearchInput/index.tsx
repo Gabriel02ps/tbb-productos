9675fc519bbb06c27d0e9fe62f5b56cd311e4b59
Gabriel02ps <gabriel02ps@gmail.com>
2023-12-09 13:15:33 -0300

    chore: create searchInput component and change header width

---
+++ src/components/SearchInput/index.tsx
@@ -0,0 +1,22 @@
+import styles from "./styles.module.scss";
+
+type SearchInputProps = {
+  placeholder: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export default function SearchInput({ placeholder, value, onChange }: SearchInputProps) {
+  return (
+    <div className={styles.inputSearch}>
+    <input
+      placeholder={placeholder}
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
+    <img src="/icons/search.svg" alt="Ícone de pesquisa" />
+  </div>
+  )
+}
+
