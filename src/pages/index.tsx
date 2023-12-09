e62af6cd3e0b2b8e556b7e66035f185a5d688b66
Gabriel02ps <gabriel02ps@gmail.com>
2023-12-09 13:16:51 -0300

    chore: add data fetch with axios and home products structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,76 @@
+import { useEffect, useState } from "react";
+import SearchInput from "../components/SearchInput";
 import styles from "../styles/home.module.scss";
+import axios, { AxiosResponse, AxiosError } from "axios";
+
+type ProductImages = {
+  alt: string;
+  asset: {
+    url: string;
+  };
+};
+
+export type Product = {
+  name: string;
+  shortDescription: string;
+  id: string;
+  images: ProductImages[];
+  category: {
+    _id: string;
+    name: string;
+  };
+};
 
 export default function Home() {
+  const [products, setProducts] = useState<Product[] | null>(null);
+  const [inputSearch, setInputSearch] = useState<string>("");
+  const handleInputSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log('inputSearch', inputSearch);
+    
+  };
+  
+  const getProducts = async () => {
+    await axios
+      .get<Product[]>("/db/products.json")
+      .then((response: AxiosResponse) => {
+        setProducts(response.data.data.nodes);
+      })
+      .catch((error: AxiosError) => console.log(error));
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
-    <div className={styles.container}>Productos Rexona</div>
+    <div className={styles.container}>
+      <div className={styles.grid}>
+        <div className={styles.form}>
+        <h2>Busca tus productos</h2>
+          <form onSubmit={handleInputSearch} className={styles.search}>
+              <SearchInput
+                placeholder="Busca aquí..."	
+                value={inputSearch}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setInputSearch(e.target.value);
+                }}
+              />
+          </form>
+        </div>
+
+        <div>
+          <span>Resultados: {products ? products.length : 0}</span>
+          <ul>
+            {products?.map((product, index) => (
+              <li key={index}>{product.name}</li>
+            ))}
+          </ul>
+        </div>
+        
+      </div>
+
+    </div>
   )
 }
